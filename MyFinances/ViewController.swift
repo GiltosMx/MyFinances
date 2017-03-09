920a3da8cdcf4079478fa4d9728bae0b50e1b037
GiltosMx <adriantoscanop@gmail.com>
2017-03-09 16:40:22 -0600

    Initial Commit

---
+++ MyFinances/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFinances
+//
+//  Created by Adrián Toscano Prieto on 09/03/17.
+//  Copyright © 2017 Adrián Toscano Prieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
